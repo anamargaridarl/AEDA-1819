9e4083a9d1718fa047c74cd285c5254181a88137
Unknown <anamargaridarl@gmail.com>
2018-10-14 18:06:03 +0100

    blba

--- tp3/Debug/src/subdir.mk
+++ tp3/Debug/src/subdir.mk
@@ -26,7 +26,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++1z -I"/home/anamargaridarl/Documents/AEDA/tp3 final/cute" -I"/home/anamargaridarl/Documents/AEDA/tp3 final/boost" -O0 -g3 -Wall -c -fmessage-length=0 -Wno-attributes -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -std=c++1z -I"/home/anamargaridarl/Documents/AEDA/AEDA-1819/tp3/cute" -I"/home/anamargaridarl/Documents/AEDA/AEDA-1819/tp3/boost" -O0 -g3 -Wall -c -fmessage-length=0 -Wno-attributes -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
